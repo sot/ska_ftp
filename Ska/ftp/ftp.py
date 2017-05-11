6a31e5ccdd36ee61a3e31f903c4c917e7861a1e5
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-05-10 21:44:45 -0400

    Wrap attempt to close ftp session on __del__ in try/catch
    self.ftp.close() seems to throw an exception if the session is gone.
    Exceptions are ignored in __del__ anyway, but this can end up printed
    to console (and, for example, ends up in the kadi test output, and I
    don't see a problem with the kadi test setup).

--- Ska/ftp/ftp.py
+++ Ska/ftp/ftp.py
@@ -81,7 +81,10 @@ class SFTP(object):
         """
         Delete object
         """
-        self.ftp.close()
+        try:
+            self.ftp.close()
+        except:
+            pass
 
     def cd(self, dirname):
         """Change to specified directory ``dirname``.
